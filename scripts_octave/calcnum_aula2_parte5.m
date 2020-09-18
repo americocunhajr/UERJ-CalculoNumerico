494b622736e68f7ccb5653c272548331ddc8de6b
Americo Cunha <36556019+americocunhajr@users.noreply.github.com>
2020-09-18 00:48:02 -0300

    Add files via upload

---
+++ scripts_octave/calcnum_aula2_parte5.m
@@ -0,0 +1,1 @@
+% Universidade do Estado do Rio de Janeiro -UERJ% Calculo Numerico% Aula 2 - Noções de Programação para Computação Científica% Prof. Americo Cunha% Prof. Augusto Barbosa% Prof. Luiz Mariano Carvalho% Profa. Nancy Baygorrea% Funções e Scripts no GNU Octaveclcclear% scripta = 2.0b = 1.0c = 1.0[x1,x2] = eq2(a,b,c)
---
+++ scripts_octave/eq2.m
@@ -0,0 +1,1 @@
+% funçãofunction [x1,x2] = eq2(a,b,c)  x1 = (-b + sqrt(b^2 - 4*a*c))/(2*a);  x2 = (-b - sqrt(b^2 - 4*a*c))/(2*a);endfunction
