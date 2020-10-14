7d917eeb51122e2e143a6f2fa171949a4315b429
Americo Cunha <36556019+americocunhajr@users.noreply.github.com>
2020-10-14 02:00:30 -0300

    Add files via upload

--- scripts_octave/newton_example1.m
+++ scripts_octave/newton_example1.m
@@ -3,13 +3,16 @@ clc
 clear
 
 % Example 1
-x0 = 0.5; tol = 1.0e-9; maxiter = 10;
+x0 = 0.5; tol = 1.0e-9; maxiter = 100;
  f = @(x) x*exp(x)-1;
-df = @(x) x*(1+exp(x));
+df = @(x) exp(x)*(1+x);
+disp('Newton:')
 root1 = newton(f,df,x0,tol,maxiter);
 
+disp('Fixed Point:')
 g = @(x) exp(-x);
 root2 = fixedpoint(g,x0,tol,maxiter);
 
+disp('Bisection:')
 a = 0.0; b = 1.0;
 root3 = bisection(f,a,b,tol);
--- scripts_octave/newton_example2.m
+++ scripts_octave/newton_example2.m
@@ -3,10 +3,13 @@ clc
 clear
 
 % Example 2
-tol = 1.0e-9; maxiter = 10;
+tol = 1.0e-9; maxiter = 100;
  f = @(x) x^2 - 2;
 df = @(x) 2*x;
 x0 = 2.0;
+disp(['Initial guess x0 = ',num2str(x0)])
 root1 = newton(f,df,x0,tol,maxiter);
+
 x0 = -2.0;
+disp(['Initial guess x0 = ',num2str(x0)])
 root2 = newton(f,df,x0,tol,maxiter);
