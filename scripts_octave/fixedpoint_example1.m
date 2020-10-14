2e5801c72a62ac7444b87f943f0d552650af0caf
Americo Cunha <36556019+americocunhajr@users.noreply.github.com>
2020-10-14 01:59:45 -0300

    Add files via upload

--- scripts_octave/fixedpoint_example1.m
+++ scripts_octave/fixedpoint_example1.m
@@ -1,9 +1,8 @@
 
 clc
 clear
-close all
 
 % Example 1
-x0 = 0.5; tol = 1.0e-3; maxiter = 100;
+x0 = 0.5; tol = 1.0e-9; maxiter = 100;
 g1 = @(x) exp(-x);
 root1 = fixedpoint(g1,x0,tol,maxiter);
--- scripts_octave/fixedpoint_example2.m
+++ scripts_octave/fixedpoint_example2.m
@@ -1,9 +1,8 @@
 
 clc
 clear
-close all
 
 % Example 2
-x0 = 0.5; tol = 1.0e-3; maxiter = 100;
+x0 = 0.5; tol = 1.0e-9; maxiter = 100;
 g2 = @(x) (1+x)/(1+exp(x));
 root2 = fixedpoint(g2,x0,tol,maxiter);
